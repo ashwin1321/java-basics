6957a5f621dad6e61f0773de782fed0a56ad4034
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-11 08:51:49 +0545

    switch case

--- src/SwitchCase.java
+++ src/SwitchCase.java
@@ -1,15 +1,15 @@
-public class SwitchCase {
-    Switch(var) {
-        Case C1:
-        //Code;
-        break;
-        Case C2:
-        //Code;
-        break;
-        Case C3:
-        //Code
-        break;
-        default:
-        //Code
-
-    }
+//public class SwitchCase {
+//    Switch(var) {
+//        Case C1:
+//        //Code;
+//        break;
+//        Case C2:
+//        //Code;
+//        break;
+//        Case C3:
+//        //Code
+//        break;
+//        default:
+//        //Code
+//
+//    }
