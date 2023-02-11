04fd29e69a8fbfc78344e71f2fd614386a17016d
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-11 08:46:58 +0545

    strings methods

---
+++ src/StringMethods.java
@@ -0,0 +1,45 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String name = "Ashwin";
+        // System.out.println(name);
+        int value = name.length();
+        //System.out.println(value);
+
+        //String lstring = name.toLowerCase();
+        //System.out.println(lstring);
+
+        //String ustring = name.toUpperCase();
+        //System.out.println(ustring);
+
+        //String nonTrimmedString = "     Ashwin     ";
+        //System.out.println(nonTrimmedString);
+
+        //String trimmedString = nonTrimmedString.trim();
+        //System.out.println(trimmedString);
+
+        //System.out.println(name.substring(1));
+        //System.out.println(name.substring(1,5));
+
+        //System.out.println(name.replace('r', 'p'));
+        //System.out.println(name.replace("r", "ier"));
+
+        //System.out.println(name.startsWith("Ash"));
+        //System.out.println(name.endsWith("win"));
+
+        //System.out.println(name.charAt(4));
+
+        //String modifiedName = "Ashwwiinnnnnww";
+        //System.out.println(modifiedName.indexOf("ww"));
+        //System.out.println(modifiedName.indexOf("ww", 4));
+        //System.out.println(modifiedName.lastIndexOf("ww", 7));
+
+        //System.out.println(name.equals("Ashwin"));
+        System.out.println(name.equalsIgnoreCase("AshWin"));
+
+        System.out.println("I am escape sequence\tdouble quote");
+
+
+
+
+    }
+}
